a4e88028008c1a82a9d33ad7ccc37c107195ed19
milliorn <scottmilliorn@gmail.com>
2022-04-23 22:35:36 -0700

    results

--- Rust-match-made-in-hell/src/main.rs
+++ Rust-match-made-in-hell/src/main.rs
@@ -1,11 +1,20 @@
+use rand::Rng;
+
 fn is_good() -> bool {
     true
 }
 
 fn main() {
-    let msg = match is_good() {
+    let good = match is_good() {
         true => "It is good",
         false => "It isn't good, yet",
     };
-    print!("{msg}")
+    println!("{good}");
+
+    let results = match rand::thread_rng().gen_range(0..=10) {
+        10 => "Overwhelming victory",
+        5.. => "Victory",
+        _ => "Defeat",
+    };
+    println!("{results}")
 }

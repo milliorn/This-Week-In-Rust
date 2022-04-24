37fc1838851d0eff8c9b28f3f7d20acaf2e4d2af
milliorn <scottmilliorn@gmail.com>
2022-04-23 23:08:16 -0700

    crossbeam parking_lot

--- Rust-match-made-in-hell/src/main.rs
+++ Rust-match-made-in-hell/src/main.rs
@@ -1,24 +1,39 @@
+use parking_lot::Mutex;
 use rand::Rng;
 
+#[derive(Clone, Copy)]
 pub enum Protection {
-    Secure,
-    #[deprecated = "using secure mode everywhere is now strongly recommended"]
+    Secure(SecureVersion),
     Insecure,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum SecureVersion {
+    V1,
+    V2,
+    V2_1,
 }
 
 fn is_good() -> bool {
     true
 }
 
-fn process(prot: Protection) {
+fn process(prot: &Protection) {
     match prot {
-        Protection::Secure => {
-            println!("No hackers")
+        Protection::Secure(version) => {
+            println!("Hacker-safe thanks to protocol v{version:?}");
         }
-        #[allow(deprecated)]
         Protection::Insecure => {
-            println!("Come in")
+            println!("Come on in");
         }
+    }
+}
+
+struct NoisyDrop;
+
+impl Drop for NoisyDrop {
+    fn drop(&mut self) {
+        println!("dropping!")
     }
 }
 
@@ -36,5 +51,37 @@ fn main() {
     };
     println!("{results}");
 
-    process(Protection::Insecure)
+    let prot = Protection::Secure(SecureVersion::V2_1);
+    process(&prot);
+    process(&prot);
+
+    let counter = Mutex::new(0_u64);
+
+    crossbeam::scope(|s| {
+        for _ in 0..3 {
+            s.spawn(|_| {
+                for _ in 0..100_000 {
+                    *counter.lock() += 1;
+                }
+            });
+        }
+    })
+    .unwrap();
+
+    /*
+        let counter = counter.into_inner();
+        println!("final count: {counter}");
+    */
+    let mut guard = counter.lock();
+
+    let mutable_ref: &mut u64 = &mut guard;
+    *mutable_ref = 42;
+
+    let immutable_ref: &u64 = &guard;
+    dbg!(immutable_ref);
+
+    let nd = NoisyDrop;
+    println!("before drop...");
+    drop(nd);
+    println!("after drop!")
 }

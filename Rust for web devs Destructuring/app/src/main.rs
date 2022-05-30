8d4b24348eeca1b7cce0edd3e24f3912bf68c308
milliorn <scottmilliorn@gmail.com>
2022-05-30 14:14:31 -0700

    destructure types

--- Rust for web devs Destructuring/app/src/main.rs
+++ Rust for web devs Destructuring/app/src/main.rs
@@ -1,3 +1,19 @@
+struct User {
+    name: String,
+    city: String,
+    country: String,
+}
+
+fn print_user(user: &User) {
+    let User {
+        name,
+        city,
+        country,
+    } = user;
+
+    println!("User {} is from {}, {}", name, city, country);
+}
+
 fn my_function(data: &(u32, &str)) {
     let (my_num, my_str) = data;
     println!("my_num: {}, my_str: {}", my_num, my_str);
@@ -8,4 +24,12 @@ fn main() {
 
     let data = (4, "OK");
     my_function(&data);
+
+    let user = User {
+        name: "Time".to_string(),
+        city: "Ottawa".to_string(),
+        country: "Canada".to_string(),
+    };
+
+    print_user(&user);
 }
